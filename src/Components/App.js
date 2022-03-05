eb72054f0f3b8ce270eb94c09530131c59acdc53
crashing booth <jholtzkener@hotmail.com>
2022-03-05 08:18:59 -0500

    moves sequencer state to context

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,33 +1,12 @@
 import * as Tone from "tone";
-import React, { useState, useRef, useContext } from 'react';
+import React, { useState, useRef, useContext, useEffect } from 'react';
 import {sequencerContext} from '../Providers/sequencerContext';
 import './App.css';
 import DotRow from './DotRow';
 
 function App() {
-  const bPat1 = [1,0,0,0, 1,1,0,0, 1,0,0,0, 1,1,0,0];
-  const bPat2 = [0,0,1,0, 0,0,1,1, 0,0,1,0, 0,0,1,1];
-  const bPat3 = [1,0,0,1, 0,0,1,0, 0,1,0,0, 1,0,1,0];
-  const bPat4 = [0,0,0,0, 0,0,0,0, 0,0,0,1, 0,1,0,1];
-  const patterns = [bPat1, bPat2];
-
+  const { lines, resetLines } = useContext(sequencerContext);
   const highlightedIndex = useRef(0);
-  const { resetLines } = useContext(sequencerContext);
-
-  const synthA = new Tone.Sampler({
-	urls: {	C3: "audio/kick.mp3" },
-	baseUrl: "https://raw.githubusercontent.com/crashingbooth/static-step-sequencer/main/"}).toDestination();
-  const synthB = new Tone.Sampler({
-	urls: { C3: "audio/hh.wav" },
-	baseUrl: "https://raw.githubusercontent.com/crashingbooth/static-step-sequencer/main/"}).toDestination();
-  const samples = [synthA, synthB]
-
-  const lines = [
-    {pattern: bPat1, sample: synthA},
-    {pattern: bPat2, sample: synthB},
-  ]
-  resetLines(lines);
-
 
   let i = 0;
   const play = () => {
--- src/Providers/sequencerContext.js
+++ src/Providers/sequencerContext.js
@@ -1,13 +1,35 @@
+import * as Tone from "tone";
 const { useState, createContext } = require("react");
 
 export const sequencerContext = createContext();
 
 const PatternProvider = props => {
-  const [lines, setLines] = useState();
+  const bPat1 = [1,0,0,0, 1,1,0,0, 1,0,0,0, 1,1,0,0];
+  const bPat2 = [0,0,1,0, 0,0,1,1, 0,0,1,0, 0,0,1,1];
+  const bPat3 = [1,0,0,1, 0,0,1,0, 0,1,0,0, 1,0,1,0];
+  const bPat4 = [0,0,0,0, 0,0,0,0, 0,0,0,1, 0,1,0,1];
+  const patterns = [bPat1, bPat2];
+
+
+  const synthA = new Tone.Sampler({
+	urls: {	C3: "audio/kick.mp3" },
+	baseUrl: "https://raw.githubusercontent.com/crashingbooth/static-step-sequencer/main/"}).toDestination();
+  const synthB = new Tone.Sampler({
+	urls: { C3: "audio/hh.wav" },
+	baseUrl: "https://raw.githubusercontent.com/crashingbooth/static-step-sequencer/main/"}).toDestination();
+  const samples = [synthA, synthB]
+
+  const sampleLines = [
+    {pattern: bPat1, sample: synthA},
+    {pattern: bPat2, sample: synthB},
+  ]
+  const [lines, setLines] = useState(sampleLines);
 
 
   const resetLines = (newLines) => {
+    console.log("setting lines");
     setLines(newLines)
+    console.log(newLines);
   };
 
 
