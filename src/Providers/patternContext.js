16a035686a274bfa02faef951d2dcf71310f8e04
crashing booth <jholtzkener@hotmail.com>
2022-03-12 11:33:25 -0500

    adds tracks labels

--- src/Providers/patternContext.js
+++ src/Providers/patternContext.js
@@ -1,52 +1,37 @@
 import * as Tone from "tone";
-import React, { useState, useContext, createContext, useEffect} from 'react';
-import {audioResources} from '../audioUrls';
+import React, { useState, useContext, createContext, useEffect } from "react";
+import { audioResources } from "../audioUrls";
 
 export const patternContext = createContext();
 
-const PatternProvider = props => {
-  const bPat1 = [1,0,0,0, 1,1,0,0, 1,0,0,0, 1,1,0,0];
-  const bPat2 = [0,0,1,0, 0,0,1,1, 0,0,1,0, 0,0,1,1];
-  const bPat3 = [1,0,0,1, 0,0,1,0, 0,1,0,0, 1,0,1,0];
-  const bPat4 = [0,0,0,0, 0,0,0,0, 0,0,0,1, 0,1,0,1];
-  const bPat5 = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
-  const bPats = [bPat1,bPat2,bPat3,bPat4,bPat5,bPat5,bPat5];
+const PatternProvider = (props) => {
+  const bPat1 = [1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0];
+  const bPat2 = [0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1];
+  const bPat3 = [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0];
+  const bPat4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1];
+  const bPat5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const bPats = [bPat1, bPat2, bPat3, bPat4, bPat5, bPat5, bPat5];
 
-  // const sampleLines = [
-  //   {pattern: bPat1, sample: audioResources[0].synth, muteStatus: false},
-  //   {pattern: bPat2, sample: audioResources[1].synth, muteStatus: false},
-  //   {pattern: bPat3, sample: audioResources[2].synth, muteStatus: false},
-  //   {pattern: bPat4, sample: audioResources[3].synth, muteStatus: false},
-  //   {pattern: bPat5, sample: audioResources[4].synth, muteStatus: false},
-  // ]
-const sampleLines = audioResources.map((audioResource, i) => {
-  return {
-    pattern: bPats[i],
-    sample: audioResource.synth,
-    muteStatus: false
-  };
-});
-const [lines, setLines] = useState(sampleLines);
+  const sampleLines = audioResources.map((audioResource, i) => {
+    return {
+      pattern: bPats[i],
+      sample: audioResource.synth,
+      muteStatus: false,
+      displayName: audioResource.displayName,
+    };
+  });
 
-
-  const bundleProperties = (patterns, synths, muteStatuses) => {
-    let result = [];
-    patterns.forEach((pattern, i) => {
-      let line = {pattern: patterns[i], sample: synths[i], muteStatus: muteStatuses[i]}
-      result.push(line);
-    });
-    return result;
-  }
+  const [lines, setLines] = useState(sampleLines);
 
   const logLines = () => {
     console.log(lines);
-  }
+  };
 
   const toggleMuteForLine = (lineNumber) => {
     const prev = [...lines];
     prev[lineNumber].muteStatus = !lines[lineNumber].muteStatus;
     setLines(prev);
-  }
+  };
 
   const generateRandomLine = (size) => {
     let res = [];
@@ -54,32 +39,38 @@ const [lines, setLines] = useState(sampleLines);
       res.push(Math.round(Math.random()));
     }
     return res;
-  }
+  };
 
   const setLine = (lineNumber, newPattern) => {
     const prev = [...lines];
     prev[lineNumber].pattern = newPattern;
     setLines(prev);
-  }
+  };
 
-  const randomizeLine = (lineNumber) =>  {
+  const randomizeLine = (lineNumber) => {
     setLine(lineNumber, generateRandomLine(lines[0].pattern.length));
-  }
+  };
 
   const toggleDot = (lineNumber, dotNumber) => {
     const pat = [...lines[lineNumber].pattern];
     pat[dotNumber] = !pat[dotNumber];
     setLine(lineNumber, pat);
-  }
+  };
 
-  const provideData = { lines, setLines, logLines, toggleMuteForLine, randomizeLine , toggleDot};
+  const provideData = {
+    lines,
+    setLines,
+    logLines,
+    toggleMuteForLine,
+    randomizeLine,
+    toggleDot,
+  };
 
   return (
     <patternContext.Provider value={provideData}>
       {props.children}
     </patternContext.Provider>
   );
-
 };
 
 export default PatternProvider;
