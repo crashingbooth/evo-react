dc0ab6a3748d786266bfcc7af9670f4654789422
crashing booth <jholtzkener@hotmail.com>
2022-03-12 08:22:10 -0500

    moves TrackEventSection to separate component

--- src/Components/Track.js
+++ src/Components/Track.js
@@ -1,28 +1,19 @@
 import React, { useState, useContext, useEffect } from 'react';
-import Dot from './Dot';
 import MuteButton from './MuteButton';
 import RandomButton from './RandomButton';
-import {patternContext} from '../Providers/patternContext';
+import TrackEventSection from './TrackEventSection';
 import '../Styles/Track.css';
 
 export default function Track(props) {
-  const { lines } = useContext(patternContext);
-  const [pattern, setPattern] = useState([]);
-
-  useEffect(() => {
-    const line = lines[props.lineNumber].pattern;
-    setPattern(line);
-  },[lines]);
 
   return (
-
     <div className='wrapper'>
       <MuteButton lineNumber={props.lineNumber} />
       <RandomButton lineNumber={props.lineNumber} />
       <div className="spacer"/>
       <div className="v-line"/>
       <div className="spacer"/>
-      {pattern.map((beat, index) => <Dot active={beat} key={index} id={index} lineNumber={props.lineNumber} isMute={lines[props.lineNumber].muteStatus}/>)}
+      <TrackEventSection lineNumber={props.lineNumber}/>
     </div>
   )
 }
---
+++ src/Components/TrackEventSection.js
@@ -0,0 +1,30 @@
+import React, { useState, useContext, useEffect } from 'react';
+import Dot from './Dot';
+import { patternContext } from "../Providers/patternContext";
+import "../Styles/Track.css";
+
+function TrackEventSection(props) {
+  const { lines } = useContext(patternContext);
+  const [pattern, setPattern] = useState([]);
+
+  useEffect(() => {
+    const line = lines[props.lineNumber].pattern;
+    setPattern(line);
+  },[lines]);
+
+  return (
+    <>
+      {pattern.map((beat, index) => (
+        <Dot
+          active={beat}
+          key={index}
+          id={index}
+          lineNumber={props.lineNumber}
+          isMute={lines[props.lineNumber].muteStatus}
+        />
+      ))}
+    </>
+  );
+}
+
+export default TrackEventSection;
