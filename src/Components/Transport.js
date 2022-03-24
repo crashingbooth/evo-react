bc5e7560bb16cd15729eac157a86686de0f019b6
crashing booth <jholtzkener@hotmail.com>
2022-03-24 07:30:39 -0400

    removes useEffect for Playing in transport

--- src/Components/Transport.js
+++ src/Components/Transport.js
@@ -8,15 +8,10 @@ import '../Styles/transport.css';
 function Transport() {
   const {play, stop, playing} = useContext(sequencerContext);
   const {savePattern, addTrack, undo, redo, canUndo, canRedo} = useContext(patternContext);
-  const [isPlaying, setIsPlaying] = useState(playing.current);
-
-  useEffect(() => {
-    setIsPlaying(playing.current);
-  },[playing.current]);
 
   return (
     <div className="transport-wrapper section-wrapper">
-      <button className={`transport-button ${isPlaying ? "playing" : ""}`} onClick={play}>Play</button>
+      <button className={`transport-button ${playing.current ? "playing" : ""}`} onClick={play}>Play</button>
       <button className="transport-button" onClick={stop}>Stop</button>
       <button className="transport-button" onClick={savePattern}>Save Lines</button>
       <button className="transport-button" onClick={addTrack}>Add Track</button>
