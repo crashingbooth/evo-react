1b070cb336b904c7ebf250f24104094b94ec6127
crashing booth <jholtzkener@hotmail.com>
2022-03-25 07:04:09 -0400

    adds upload button to transport

--- src/Components/Transport.js
+++ src/Components/Transport.js
@@ -5,13 +5,20 @@ import '../Styles/styles.css';
 import '../Styles/transport.css';
 
 function Transport() {
-  const {savePattern, addTrack, undo, redo, canUndo, canRedo, play, stop, playing} = useContext(patternContext);
+  const inputFile = useRef(null)
+  const {savePattern,loadPatterns, addTrack, undo, redo, canUndo, canRedo, play, stop, playing} = useContext(patternContext);
+
+  const onButtonClick = () => {
+     inputFile.current.click();
+    };
 
   return (
     <div className="transport-wrapper section-wrapper">
       <button className={`transport-button ${playing.current ? "playing" : ""}`} onClick={play}>Play</button>
       <button className="transport-button" onClick={stop}>Stop</button>
       <button className="transport-button" onClick={savePattern}>Save Lines</button>
+      <input type='file' id='file' ref={inputFile} style={{display: 'none'}}/>
+      <button className="transport-button" onClick={onButtonClick}>Load Lines</button>
       <button className="transport-button" onClick={addTrack}>Add Track</button>
       <button className="transport-button" disabled={!canUndo} onClick={undo}>Undo</button>
       <button className="transport-button" disabled={!canRedo} onClick={redo}>Redo</button>
